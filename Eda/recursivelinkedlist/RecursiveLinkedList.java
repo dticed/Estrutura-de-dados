ae694300de978d28b561c256a8d634c179bca01f
dticed <lucaspierrealencar@gmail.com>
2016-04-17 22:51:14 -0300

    Teste de commit

---
+++ Eda/recursivelinkedlist/RecursiveLinkedList.java
@@ -0,0 +1,14 @@
+package recursivelinkedlist;
+
+public class RecursiveLinkedList {
+	
+	private Node head;
+	private Node tail;
+	
+	public RecursiveLinkedList() {
+		this.head = null;
+		this.tail = null;
+		
+	}
+
+}
